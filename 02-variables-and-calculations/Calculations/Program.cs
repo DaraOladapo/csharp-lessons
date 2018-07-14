32cbbe83f88f0e7743a98a2a02b0cece1bc42ce2
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-14 16:34:30 +0100

    Addition and subtraction

--- 02-variables-and-calculations/Calculations/Program.cs
+++ 02-variables-and-calculations/Calculations/Program.cs
@@ -6,7 +6,16 @@ namespace Calculations
     {
         static void Main(string[] args)
         {
-           
+            //on some int
+            int firstValue = 25, secondValue = 15;
+            int addedValue = firstValue + secondValue;
+            int subtractedValue = secondValue - firstValue;
+            int secondSubtractedValue = firstValue - secondValue;
+            //addition
+            Console.WriteLine($"{firstValue}+{secondValue}={addedValue}");
+            //substractions
+            Console.WriteLine($"{secondValue}-{firstValue}={subtractedValue}");
+            Console.WriteLine($"{firstValue}-{secondValue}={secondSubtractedValue}");
         }
     }
 }
