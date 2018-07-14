78ebc6080f43ed513c9398cf4f99a97fc32e6a0c
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-14 19:04:23 +0100

    stago

---
+++ 02-variables-and-calculations/OperatorsAndOperands/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OperatorsAndOperands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
