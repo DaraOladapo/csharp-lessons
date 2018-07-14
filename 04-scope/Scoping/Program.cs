af594d86e7cdecbd953e02907f0fc2cd0b0d90aa
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-15 00:34:49 +0100

    scoping done

--- 04-scope/Scoping/Program.cs
+++ 04-scope/Scoping/Program.cs
@@ -4,10 +4,27 @@ namespace Scoping
 {
     class Program
     {
+        //class scope: called FIELDS
+        string classLevelString = "I am sitting on the class level.";
         
         static void Main(string[] args)
         {
+            //method level scope
+            string methodLevelScope = "I am sitting at the method level.";
             Console.WriteLine("Hello World!");
+
+            {
+                //block level scope
+                string blockLevelScope = "I am only visible in this block";
+                Console.WriteLine(blockLevelScope);
+            }
+            Console.WriteLine(methodLevelScope);
+           
+            
+        }
+        void MyMethod()
+        {
+            Console.WriteLine(classLevelString);
         }
     }
 }
