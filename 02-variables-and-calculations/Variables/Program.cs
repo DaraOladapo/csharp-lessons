a4ab0f97a8a0419866c6a4d3b4db80ffec0baaaf
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-14 16:03:32 +0100

    explaining variables

--- 02-variables-and-calculations/Variables/Program.cs
+++ 02-variables-and-calculations/Variables/Program.cs
@@ -6,7 +6,20 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            
+            //declaration
+            int intValue;
+
+            //assignment
+            intValue=225;
+            Console.WriteLine($"intValue={intValue}");
+
+            //declare and assign
+            double doubleValue=52.65;
+            Console.WriteLine($"doubleValue={doubleValue}");
+
+            //assigning another value
+            doubleValue=66.5
+            Console.WriteLine($"doubleValue's new value  = {doubleValue}");
         }
     }
 }
