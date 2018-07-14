059bcb7bf930c8a7ea041ebfa35b91abb34c1520
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-14 18:57:46 +0100

    added one more method

--- 03-debugging/Debugging/Program.cs
+++ 03-debugging/Debugging/Program.cs
@@ -14,10 +14,13 @@ namespace Debugging
             //Console.WriteLine($"{secondValue}/{firstValue}={anotherDividedValue}");
             //we declared int. Let's switch things a bit.
             int firstValue = 30, secondValue = 15;
-            //method 1
+            //method 1: Casting
             double castedDivided = (double)secondValue / (double)firstValue;
-            Console.WriteLine($"{secondValue}/{firstValue}={castedDivided}");//method 1
-            //method 2
+            Console.WriteLine($"{secondValue}/{firstValue}={castedDivided}");
+            //method 2:Conversion
+            double convertDivided = Convert.ToDouble(secondValue) / Convert.ToDouble(firstValue);
+            Console.WriteLine($"{secondValue}/{firstValue}={convertDivided}");
+            //method 3:Parsing
             double parsedDivided = double.Parse(secondValue.ToString()) / double.Parse(firstValue.ToString());
             Console.WriteLine($"{secondValue}/{firstValue}={parsedDivided}");
 
