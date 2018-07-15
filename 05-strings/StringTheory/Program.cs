c43a0da183d5f12ce372bc7a3b02a750c486c804
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-15 13:04:37 +0100

    Introducing Escape sequence

---
+++ 05-strings/StringTheory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringTheory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Theory = "The theory of strings states that strings are strings";
+            Console.WriteLine(Theory);
+            string NewLineTheory = "The theory starting on a \nnew line\n uses an escape sequence \\n";
+            Console.WriteLine(NewLineTheory);
+            string TabTheory = "Oh yes!, I want to use a \ttab so I use the \\t";
+            Console.WriteLine(TabTheory);
+            string DoubleQuoteTheory="I am in a \"double quote\" using \\\" \\\"";
+            Console.WriteLine(DoubleQuoteTheory);
+        }
+
+    }
+}
