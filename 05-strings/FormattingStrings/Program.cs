968337b46444c25dc548b16650c6a6e52fc2ef0c
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-15 14:30:04 +0100

    string play over

---
+++ 05-strings/FormattingStrings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FormattingStrings
+{
+    public class SaleItem
+    {
+        public string Name
+        { get; set; }
+
+        public decimal Price
+        { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var item = new SaleItem { Name = "Shoes", Price = 19.95m };
+            Console.WriteLine($"{item.Name}: sells for {item.Price:C2}");
+
+
+            //ToString()
+            int someInt = 45;
+            string someString = someInt.ToString();
+            Console.WriteLine(someString);
+
+            string someSuperString = "I am some string with some string and some other string.";
+            //ToLower()
+            Console.WriteLine(someSuperString.ToLower());
+            //ToLower()
+            Console.WriteLine(someSuperString.ToUpper());
+
+            string Name = "John Smith";
+            var StartsWith = Name.StartsWith("J");
+            Console.WriteLine(StartsWith);
+            var Contains = Name.Contains("oh");
+            Console.WriteLine(Contains);
+            var ContainsUpper = Name.Contains("Oh");
+            Console.WriteLine(ContainsUpper);
+        }
+    }
+
+}
