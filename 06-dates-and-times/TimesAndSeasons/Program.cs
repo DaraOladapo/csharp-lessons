369b6c7c514dfcd02734a5a03b9e0cfb725b5481
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-16 00:15:56 +0100

    Up on dates and times

---
+++ 06-dates-and-times/TimesAndSeasons/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TimesAndSeasons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Today's full date is {DateTime.Now}");
+            Console.WriteLine($"Today's long date is {DateTime.Now.ToLongDateString()}");
+            Console.WriteLine($"Today's short date is {DateTime.Now.ToShortDateString()}");
+            Console.WriteLine($"The long time is {DateTime.Now.ToLongTimeString()}");
+            Console.WriteLine($"The time is {DateTime.Now.ToShortTimeString()}");
+
+        }
+    }
+}
