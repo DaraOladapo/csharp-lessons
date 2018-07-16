957583bfd639f71d75eb0859ffcf2964011b7336
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-16 02:21:57 +0100

    added enums and structs projects

---
+++ 08-enumerations-and-structures/Enums/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 08-enumerations-and-structures/Structs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
