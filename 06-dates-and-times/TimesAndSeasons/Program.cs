2a0dc9941065973fae7a06ab873cb8229bec282f
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-16 02:00:02 +0100

    up on arrays

--- 06-dates-and-times/TimesAndSeasons/Program.cs
+++ 06-dates-and-times/TimesAndSeasons/Program.cs
@@ -11,7 +11,6 @@ namespace TimesAndSeasons
             Console.WriteLine($"Today's short date is {DateTime.Now.ToShortDateString()}");
             Console.WriteLine($"The long time is {DateTime.Now.ToLongTimeString()}");
             Console.WriteLine($"The time is {DateTime.Now.ToShortTimeString()}");
-
         }
     }
 }
--- 07-arrays-and-collections/Arrays/Program.cs
+++ 07-arrays-and-collections/Arrays/Program.cs
@@ -6,7 +6,28 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           int[] intArray=new int[5];
+           System.Console.WriteLine($"intArray has a length of {intArray.Length}");
+           intArray[0]=12;
+           intArray[1]=11;
+           intArray[2]=1;
+           intArray[4]=85;
+           Console.WriteLine(intArray[3]);
+           Console.WriteLine(intArray[4]);
+
+            string[] someStringArray=new string[5];
+            someStringArray[4]="Hello string array item 4";
+            Console.WriteLine(someStringArray[4]);
+            Console.WriteLine(someStringArray[3]);
+
+            string stringToSplit="ABCDEF";
+            Console.WriteLine(stringToSplit[4]);
+            string spacedString="A B C D E F";
+            char[] arraySplitter={' '};
+            string[] splittedStringV1=spacedString.Split(arraySplitter);
+            string[] splittedStringV2=spacedString.Split(arraySplitter,StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(splittedStringV1[2]);
+            Console.WriteLine(splittedStringV2[5]);
         }
     }
 }
