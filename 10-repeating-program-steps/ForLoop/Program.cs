0e11df699a19c70c918ee0e825e8a09a0d0fd424
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-17 23:00:29 +0100

    FOrLoop

--- 10-repeating-program-steps/ForLoop/Program.cs
+++ 10-repeating-program-steps/ForLoop/Program.cs
@@ -6,7 +6,16 @@ namespace ForLoop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] AllStudents={"James","John", "Julian","Joseph"};
+            for(int i=0;i<AllStudents.Length;i++){
+                Console.WriteLine(AllStudents[i]);
+            }
+
+
+            int[] someValues={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+            for(int i=2;i<=someValues.Length;i++2){
+                Console.WriteLine(someValues[i];
+            }
         }
     }
 }
