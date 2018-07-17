c2a429684353d4764d99fbc7ef0457b10ef1c3dc
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-17 22:13:06 +0100

    Ternary Done

--- 09-making-choices/Ternary/Program.cs
+++ 09-making-choices/Ternary/Program.cs
@@ -6,7 +6,12 @@ namespace Ternary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("We want to check if an input value is an even number\nEnter an integer value below.");
+            string OutputMessage;
+            int InputValue=int.Parse(Console.ReadLine());
+            OutputMessage=(InputValue%2==0)?$"{InputValue} is an even number.":$"{InputValue} is an odd number.";
+            Console.WriteLine(OutputMessage);
+
         }
     }
 }
