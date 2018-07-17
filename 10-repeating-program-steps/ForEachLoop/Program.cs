64731e348616f0c7fb46cacb8cca90829f5e9517
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-17 23:04:54 +0100

    For each Loop

--- 10-repeating-program-steps/ForEachLoop/Program.cs
+++ 10-repeating-program-steps/ForEachLoop/Program.cs
@@ -6,7 +6,18 @@ namespace ForEachLoop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] AllStudents={"James","John", "Julian","Joseph"};
+            foreach (var item in AllStudents)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            int[] someValues={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+            foreach (var item in someValues)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
