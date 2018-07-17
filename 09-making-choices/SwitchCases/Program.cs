9b679a603481d7ef11f2cc47638f313f700aa2af
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-17 22:26:21 +0100

    switch over

--- 09-making-choices/SwitchCases/Program.cs
+++ 09-making-choices/SwitchCases/Program.cs
@@ -6,7 +6,23 @@ namespace SwitchCases
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("We want to use switch cases\nEnter a value");
+            int inputVal=int.Parse(Console.ReadLine());
+            switch (inputVal)
+            {
+             case 1:
+                Console.WriteLine("Your input is 1");
+             break;
+             case 2:
+            Console.WriteLine("Your input is 2");
+             break;
+             case 3:
+            Console.WriteLine("Your input is 3");
+             break;
+            default:
+            Console.WriteLine("Your input cannot be verified.");
+             break;   
+            }
         }
     }
 }
