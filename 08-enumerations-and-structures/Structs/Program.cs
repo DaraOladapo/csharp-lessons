ad3f383ae968eb94a663ce632be05a9d23c067d2
Dara Oladapo <dara.oladapo@outlook.com>
2018-07-17 21:29:46 +0100

    Struck'd OVer

--- 08-enumerations-and-structures/Structs/Program.cs
+++ 08-enumerations-and-structures/Structs/Program.cs
@@ -6,7 +6,33 @@ namespace Structs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ContactInfo contactInfo;
+            contactInfo.HomeAddress="5, Edidi Lane, Idumota, Lagos";
+            contactInfo.EmailAddress="someone@example.com";
+            contactInfo.PhoneNumber="+2348012345678";
+
+            AccountInfo accountInfo;
+            accountInfo.AccountName="12345678";
+            accountInfo.AccountName="Julius Roberts";
+            accountInfo.AccountType=AccountType.Current;
         }
+     
     }
+    struct ContactInfo
+    {
+        public string EmailAddress;
+        public string HomeAddress;
+        public string PhoneNumber;
+    }
+    struct AccountInfo
+    {
+        public string AccountName;
+        public string AccountNumber;
+        public AccountType AccountType;
+    }
+    enum AccountType
+    {
+        Savings, Current, Corporate
+    }
+   
 }
